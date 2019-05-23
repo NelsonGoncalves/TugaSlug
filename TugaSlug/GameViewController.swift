d0048d5878c171bee4378c10042a132284a14c1b
Nelson Gonçalves <nelson.m.s.goncalves@gmail.com>
2019-05-23 23:53:50 +0100

    COMMIT

---
+++ TugaSlug/GameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameViewController.swift
+//  TugaSlug
+//
+//  Created by Aluno Tmp on 23/05/2019.
+//  Copyright © 2019 NelsonTiago. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: self.view.bounds.size)
+        scene.backgroundColor = .white
+        
+        if let skView = self.view as? SKView {
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            skView.ignoresSiblingOrder = true
+            skView.presentScene(scene)
+        }
+    }
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
